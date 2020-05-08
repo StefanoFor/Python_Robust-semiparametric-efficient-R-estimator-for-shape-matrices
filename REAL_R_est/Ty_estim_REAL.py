0182c15d484a0a87122a6c636e372f83019daaba
StefanoFor <61187718+StefanoFor@users.noreply.github.com>
2020-05-08 12:03:41 +0200

    Update Ty_estim_REAL.py

--- REAL_R_est/Ty_estim_REAL.py
+++ REAL_R_est/Ty_estim_REAL.py
@@ -7,16 +7,16 @@ def compute_tyler_shape_estimator(X, Sigma_init, max_iter_fp = 40):
     K, N = X.shape
     
     Sigma_fixed_point = Sigma_init.copy()
-    sq_maha = np.empty((K, ))
+    s_maha = np.empty((K, ))
     convergence_fp = False
     ite_fp = 1
     
     while not(convergence_fp) and ite_fp < max_iter_fp:
             
         inv_Sigma_fixed_point = np.linalg.inv(Sigma_fixed_point)
-        sq_maha = ( (X @ inv_Sigma_fixed_point) * X).sum(1)
+        s_maha = ( (X @ inv_Sigma_fixed_point) * X).sum(1)
         
-        Sigma_fixed_point_new = X.T @ (N/sq_maha * X.T).T / K 
+        Sigma_fixed_point_new = X.T @ (N/s_maha * X.T).T / K 
         Sigma_fixed_point_new = Sigma_fixed_point_new/Sigma_fixed_point_new[0,0]
 
         convergence_fp = True
